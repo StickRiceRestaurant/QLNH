3f495055f0354949395bce54d1ffa460b203db66
DianNg25 <153905243+DianNg25@users.noreply.github.com>
2024-03-24 17:02:51 +0700

    Add files via upload

---
+++ QLNH/src/main/java/me/mycompany/sticky_rice_restaurant/MainRestaurant.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package me.mycompany.sticky_rice_restaurant;
+
+/**
+ *
+ * @author ACER
+ */
+public class MainRestaurant {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+        //xinchao
+        //stickrice
+        //restaurant
+    }
+}
